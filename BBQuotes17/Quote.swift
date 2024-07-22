f6b8a24663662624cfa8fd9137ec61be92d32f2a
Bayu Yuhartono <bayuyuhartono@gmail.com>
2024-07-23 00:08:51 +0700

    character page

---
+++ BBQuotes17/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  BBQuotes17
+//
+//  Created by Bayu P Yuhartono on 11/07/24.
+//
+
+import Foundation
+
+struct Quote: Decodable {
+    let quote: String
+    let character: String
+}
